e5ba0eec2f8d2fa264f8de6bde69b14529a49b24
YourBoyRory <yourboyrory@gmail.com>
2024-10-25 15:25:27 -0400

    script works, but close is un safe

---
+++ burn.sh
@@ -0,0 +1,17 @@
+burn() {
+    while true; do
+        ./bin/benchmark-launcher-cli benchmark classroom --blender-version 4.2.0 --device-type $1
+        sleep 1
+    done
+}
+
+displayGif() {
+    pqiv --fullscreen --scale-images-up ./assets/fire.gif
+    killall blender 
+    killall bash
+}
+
+burn CPU &
+burn CUDA &
+displayGif
+
