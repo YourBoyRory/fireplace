4fc64d72b8eb6b0acc01907719088d8112554b20
YourBoyRory <yourboyrory@gmail.com>
2024-11-02 11:29:19 -0400

    Made windows and linux build in different nenv to support different requirements

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$1" != "--use-system-packages" ]]; then
-    python -m venv ./venv
-    source ./venv/bin/activate
-    pip install -r requirements.txt
+    python -m venv ./venv-linux
+    source ./venv-linux/bin/activate
+    pip install -r requirements-linux.txt
 else
     echo "Using system packages"
 fi
--- scripts/build-windows.cmd
+++ scripts/build-windows.cmd
@@ -1,7 +1,7 @@
 
-python -m venv ./venv
-source ./venv/Scripts/activate.bat
-pip install -r requirements.txt
+python -m venv ./venv-windows
+source ./venv-windows/Scripts/activate.bat
+pip install -r requirements-windows.txt
 pyinstaller --windowed --onefile --name "Fireplace" --icon=assets/icon.ico --add-data "assets/icon-small.png:assets" --add-data "assets/fire.gif:assets" --add-data "opencl_workloads/opencl_stress.cl:opencl_workloads" ./Fireplace.py
 echo.
 echo Build Done
