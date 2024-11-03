885fb578453fc2ad8a85d26f9bd138998d79b6c6
YourBoyRory <yourboyrory@gmail.com>
2024-11-02 23:10:18 -0400

    added font to build script

--- scripts/build-windows.cmd
+++ scripts/build-windows.cmd
@@ -2,7 +2,7 @@
 python -m venv ./venv-windows
 source ./venv-windows/Scripts/activate.bat
 pip install -r requirements-windows.txt
-pyinstaller --windowed --onefile --name "Fireplace" --icon=assets/icon.ico --add-data "lib/OpenHardwareMonitorLib.sys:lib" --add-data "lib/OpenHardwareMonitorLib.dll:lib" --add-data "assets/icon-small.png:assets" --add-data "assets/fire.gif:assets" --add-data "opencl_workloads/opencl_stress.cl:opencl_workloads" ./Fireplace.py
+pyinstaller --windowed --onefile --name "Fireplace" --icon=assets/icon.ico --add-data "assets/font.ttf:assets" --add-data "lib/OpenHardwareMonitorLib.sys:lib" --add-data "lib/OpenHardwareMonitorLib.dll:lib" --add-data "assets/icon-small.png:assets" --add-data "assets/fire.gif:assets" --add-data "opencl_workloads/opencl_stress.cl:opencl_workloads" ./Fireplace.py
 echo.
 echo Build Done
 echo.
